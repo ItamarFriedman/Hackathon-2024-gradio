f01f4e263dde915ca270e52df4600112316edf82
OriReznikTechnion <ori.reznik@campus.technion.ac.il>
2024-06-21 01:32:16 +0300

    "good version"

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ theme = gr.themes.Base(
     secondary_hue="fuchsia",
     neutral_hue="cyan")
 
+
 def segment_story(story):
     try:
         response = openai.chat.completions.create(
@@ -30,34 +31,50 @@ def segment_story(story):
         return []
 
 
-def generate_images(sub_stories):
-    images = []
-    for story in sub_stories:
-        try:
-            response = openai.images.generate(
-                model="dall-e-2",
-                prompt=story,
-                n=1,
-                size="1024x1024"
-            )
-            image_url = response.data[0].url
-            images.append(image_url)
-        except Exception as e:
-            print(e)
-            images.append("Error generating image")
-    return images
+def generate_image(sub_story):
+    print("starting to generate image")
+    print("the current substory is:", sub_story)
+    try:
+        response = openai.images.generate(
+            model="dall-e-2",
+            prompt=sub_story,
+            n=1,
+            size="1024x1024"
+        )
+        image_url = response.data[0].url
+    except Exception as e:
+        print("Error generating image: ", e)
+        return -1
+    print("image generated successfully")
+    return image_url
+
+
+def handle_next_image(sub_stories, index):
+    print("starting to handle next image")
+    if index < len(sub_stories):
+        sub_story = sub_stories[index]
+        image_url = generate_image(sub_story)
+        index += 1
+        return sub_story, image_url, index
+    else:
+        return "No more texts to process.", None, index
 
 
 def process_story(story):
     sub_stories = segment_story(story)
     if sub_stories:
-        return generate_images(sub_stories)
+        return sub_stories
     else:
         return ["Error in story segmentation"]
 
 
 # Create a Gradio interface
 with gr.Blocks(theme=theme) as app:
+    # states for the ai part
+    sub_stories_state = gr.State([])
+    image_urls_state = gr.State([])
+    index_state = gr.State(0)
+
     gr.Markdown("## MIND CANVAS")
     gr.Image("background-image.jpeg", elem_id="background-image")
     with gr.Row(elem_id="overlay"):
@@ -70,10 +87,82 @@ with gr.Blocks(theme=theme) as app:
 
     with gr.Column():
         story_input = gr.Textbox(lines=10, placeholder="Enter a full story here...", label="Story Input")
-        submit_button = gr.Button("Generate Images")
-        output_gallery = gr.Gallery(label="Generated Images")
+        # submit button setup
+        submit_button = gr.Button("Parse Story")
 
-    submit_button.click(fn=process_story, inputs=story_input, outputs=output_gallery)
+        # Text box for wait!
+        wait_text = gr.Text(value="Waiting for Parsing...", interactive=False)
+
+        # 1st generate button setup
+        generate1_button = gr.Button("Generate Image from Next Text")
+        story1_numbering = gr.Textbox(label="Photo Numbering", placeholder="Give this photo a number!", lines=1)
+        story1_output = gr.Textbox(label="Generating Text #1", lines=3)
+        image1_output = gr.Image(label="Generated Image #1")
+
+        # 2st generate button setup
+        generate2_button = gr.Button("Generate Image from Next Text")
+        story2_numbering = gr.Textbox(label="Photo Numbering", placeholder="Give this photo a number!", lines=1)
+        story2_output = gr.Textbox(label="Generating Text #2", lines=3)
+        image2_output = gr.Image(label="Generated Image #2")
+
+        # 3st generate button setup
+        generate3_button = gr.Button("Generate Image from Next Text")
+        story3_numbering = gr.Textbox(label="Photo Numbering", placeholder="Give this photo a number!", lines=1)
+        story3_output = gr.Textbox(label="Generating Text #3", lines=3)
+        image3_output = gr.Image(label="Generated Image #3")
+
+        # 4st generate button setup
+        generate4_button = gr.Button("Generate Image from Next Text")
+        story4_numbering = gr.Textbox(label="Photo Numbering", placeholder="Give this photo a number!", lines=1)
+        story4_output = gr.Textbox(label="Generating Text #4", lines=3)
+        image4_output = gr.Image(label="Generated Image #4")
+
+        # 5st generate button setup
+        generate5_button = gr.Button("Generate Image from Next Text")
+        story5_numbering = gr.Textbox(label="Photo Numbering", placeholder="Give this photo a number!", lines=1)
+        story5_output = gr.Textbox(label="Generating Text #5", lines=3)
+        image5_output = gr.Image(label="Generated Image #5")
+
+        # 6st generate button setup
+        generate6_button = gr.Button("Generate Image from Next Text")
+        story6_numbering = gr.Textbox(label="Photo Numbering", placeholder="Give this photo a number!", lines=1)
+        story6_output = gr.Textbox(label="Generating Text #6", lines=3)
+        image6_output = gr.Image(label="Generated Image #6")
+
+        def handle_submit_click(story_input_s):
+            sub_stories = segment_story(story_input_s)
+            return sub_stories, 0, gr.Text(value="All Done!", interactive=False)
+
+        submit_button.click(fn=handle_submit_click, inputs=story_input, outputs=[sub_stories_state, index_state, wait_text])
+
+        def handle_generate_click(sub_stories, index):
+            print("starting to handle generate click")
+            sub_story, image_url, new_index = handle_next_image(sub_stories, index)
+            # if image_url:
+            #    with main_container:
+            #        gr.Text(value=sub_story, interactive=False)
+            #        gr.Image(value=image_url)
+            #        print("tried to print image and text")
+            return sub_story, image_url, new_index
+
+        generate1_button.click(fn=handle_generate_click, inputs=[sub_stories_state, index_state],
+                               outputs=[story1_output, image1_output, index_state])
+
+        generate2_button.click(fn=handle_generate_click, inputs=[sub_stories_state, index_state],
+                               outputs=[story2_output, image2_output, index_state])
+
+        generate3_button.click(fn=handle_generate_click, inputs=[sub_stories_state, index_state],
+                               outputs=[story3_output, image3_output, index_state])
+
+        generate4_button.click(fn=handle_generate_click, inputs=[sub_stories_state, index_state],
+                               outputs=[story4_output, image4_output, index_state])
+
+        generate5_button.click(fn=handle_generate_click, inputs=[sub_stories_state, index_state],
+                               outputs=[story5_output, image5_output, index_state])
+
+        generate6_button.click(fn=handle_generate_click, inputs=[sub_stories_state, index_state],
+                               outputs=[story6_output, image6_output, index_state])
+
 
 if __name__ == "__main__":
     app.launch()
